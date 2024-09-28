803bebada9e6a6141d03671b6d827cce53258796
chetanknowit <chetanamrao174@gmail.com>
2024-09-28 18:55:53 +0530

    Add main.go to verify version postgres 17

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Read credentials from environment variables
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=require", user, password, host, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select version()")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var version string
+	for rows.Next() {
+		if err := rows.Scan(&version); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Printf("version=%s\n", version)
+}
